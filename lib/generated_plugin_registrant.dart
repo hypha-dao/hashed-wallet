0fb21068061e5bfdb23916614126597f2ab60690
NIK <nheger@gmail.com>
2022-07-09 17:27:39 +0700

    updates

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -8,9 +8,13 @@
 
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_installations_web/firebase_installations_web.dart';
 import 'package:firebase_messaging_web/firebase_messaging_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:package_info_plus_web/package_info_plus_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:uni_links_web/uni_links_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -21,9 +25,13 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   FirebaseFirestoreWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FirebaseInstallationsWeb.registerWith(registrar);
   FirebaseMessagingWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
+  FlutterSecureStorageWeb.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
+  PackageInfoPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UniLinksPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,11 @@ import firebase_core
 import firebase_messaging
 import firebase_remote_config
 import firebase_storage
+import flutter_secure_storage_macos
+import flutterfire_installations
+import geolocator_apple
+import in_app_review
+import package_info_plus_macos
 import path_provider_macos
 import shared_preferences_macos
 import sqflite
@@ -21,6 +26,11 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
+  FirebaseInstallationsPlugin.register(with: registry.registrar(forPlugin: "FirebaseInstallationsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
