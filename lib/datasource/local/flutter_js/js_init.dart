14ca5731afd43c548b08286327e63340ade93807
NIK <nheger@gmail.com>
2022-07-17 13:57:25 +0700

    cleanup

--- lib/datasource/local/flutter_js/js_init.dart
+++ lib/datasource/local/flutter_js/js_init.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/services.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_js/flutter_js.dart';
 
 class JSInit {
-  JavascriptRuntime runtime = getJavascriptRuntime();
+  // JavascriptRuntime runtime = getJavascriptRuntime();
 
   // bundle-polkadot-util.js
   String testPolkadotUtil = """
@@ -15,74 +16,18 @@ class JSInit {
 
   Future<void> init() async {
     try {
-      // reset runtime for debugging...
-      runtime.dispose();
-      runtime = getJavascriptRuntime();
-
-      JavascriptRuntime.debugEnabled = true;
-
       final String polkadotUtilBundle = await rootBundle.loadString('assets/polkadot/bundles/bundle-polkadot-util.js');
 
-      // final r0 = runtime.evaluate(""" // this doesn't help
-      //   var window = {};
-      //   var globalThis = window;
-      // """);
-      // print("eval r0:");
-      // print(r0);
-
       print("loading polkadotUtilBundle ${polkadotUtilBundle.length / 1000.0}");
-      // final r1 = await runtime.evaluate(polkadotUtilBundle);
-      final r1 = await runtime.evaluateAsync(polkadotUtilBundle);
-      print("eval res:");
-      print(r1);
 
-      print(runtime.evaluate(testPolkadotUtil));
+      final webView = createWebView();
 
-//       runtime.evaluate("""
-// console.log("show svc: "+service);
-// console.log("show svc: "+service.init);
-// service.init("wss://ournode.hashed.io");
-//       """);
+      // final r1 = await runtime.evaluateAsync(polkadotUtilBundle);
+      // print("eval res:");
+      // print(r1);
 
-// this works perfectly
-      //   print("res1 start");
-      //   final res1 = runtime.evaluate('''
-      //   const init = ({endpoint}) => {
-      //     console.log("mock JS CONSOLE init"+endpoint);
-      //   };
-      //   const service = {
-      //     init
-      //   }
-      // ''');
-      //   print(res1);
+      // print(runtime.evaluate(testPolkadotUtil));
 
-      //this.polka.service.init({
-      //  endpoint: "wss://n1.hashed.systems"
-      //});
-
-      // call to init can be made into a parameterized "connect" api function
-      // final rruleIsLoaded = runtime.evaluate('''
-      //   var loaded = (typeof polka == 'undefined') ?
-      //   "0" : "1"; loaded;
-      //   ''').stringResult;
-      // print("code is Loaded $rruleIsLoaded");
-
-      //   final res = await runtime.evaluateAsync('''
-      //     // import { ApiPromise, WsProvider } from "@polkadot/api";
-      //     // const { Keyring } = require("@polkadot/keyring");
-
-      //     console.log("global "+ globalThis);
-      //     //console.log("XX "+ polka);
-
-      //     // provider = new globalThis.WsProvider("wss://n1.hashed.systems");
-
-      //     // var keys = Object.keys( global );
-      //     //keys.forEach((k) => console.log(k + ":" + JSON.stringify(global[k], null, 2)));
-
-      //     console.log("window keys: "+JSON.stringify(Object.keys( globalThis ), null, 2));
-
-      //     // console.log("service: "+this.service);
-      // ''');
       //   print("init done ");
       //   print("result: $res");
 
@@ -90,5 +35,51 @@ class JSInit {
       print("Error: $err");
       rethrow;
     }
+  }
+
+  HeadlessInAppWebView createWebView() {
+    return HeadlessInAppWebView(
+      initialUrlRequest: URLRequest(url: Uri.parse("https://github.com/flutter")),
+      onWebViewCreated: (controller) {
+        print("webview create");
+        // final snackBar = const SnackBar(
+        //   content: Text('HeadlessInAppWebView created!'),
+        //   duration: Duration(seconds: 1),
+        // );
+        // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
+      onConsoleMessage: (controller, consoleMessage) {
+        print("Console Message: ${consoleMessage.message}");
+        // final snackBar = SnackBar(
+        //   content: Text('Console Message: ${consoleMessage.message}'),
+        //   duration: Duration(seconds: 1),
+        // );
+        // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
+      onLoadStart: (controller, url) async {
+        print("onLoadStart: $url");
+        // final snackBar = SnackBar(
+        //   content: Text('onLoadStart $url'),
+        //   duration: Duration(seconds: 1),
+        // );
+        // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+        // setState(() {
+        //   this.url = url?.toString() ?? '';
+        // });
+      },
+      onLoadStop: (controller, url) async {
+        print("onLoadStop: $url");
+        // final snackBar = SnackBar(
+        //   content: Text('onLoadStop $url'),
+        //   duration: Duration(seconds: 1),
+        // );
+        // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+        // setState(() {
+        //   this.url = url?.toString() ?? '';
+        // });
+      },
+    );
   }
 }
