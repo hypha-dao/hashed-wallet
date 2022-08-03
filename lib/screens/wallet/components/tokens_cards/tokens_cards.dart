53546ee2fe09bd7778554314b4ff970e886ddb0d
NIK <nheger@gmail.com>
2022-08-03 14:38:43 +0700

    remove debug code

--- lib/screens/wallet/components/tokens_cards/tokens_cards.dart
+++ lib/screens/wallet/components/tokens_cards/tokens_cards.dart
@@ -70,93 +70,18 @@ class _TokenCardsState extends State<TokenCards> with AutomaticKeepAliveClientMi
                     children: [
                       Expanded(child: WalletButtons(onPressed: () {}, title: 'Send')),
                       const SizedBox(width: 16),
+                      // ignore: prefer_const_constructors
                       Expanded(
+                        // ignore: prefer_const_constructors
                         child: WalletButtons(
-                          title: 'Start Service',
-                          onPressed: () async {
-                            // testing substrate service - leave this for now
-                            print("startService..");
-
-                            await polkadotRepository.startService();
-                            print("start service finished...");
-                          },
+                          title: 'Receive',
+                          onPressed: null,
                           buttonType: ButtonsType.receiveButton,
                         ),
                       )
                     ],
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: WalletButtons(
-                          title: 'Create acct',
-                          onPressed: () {
-                            print("Create account");
-                            polkadotRepository.createKey();
-                          },
-                        ),
-                      ),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: WalletButtons(
-                          title: 'Stop Service',
-                          onPressed: () async {
-                            // testing substrate service - leave this for now
-                            print("disable web view");
-
-                            print("Stop service.");
-
-                            await polkadotRepository.stopService();
-
-                            print("isRunning ${polkadotRepository.isConnected}");
-                          },
-                          buttonType: ButtonsType.receiveButton,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: WalletButtons(
-                          title: 'Test Import',
-                          onPressed: () {
-                            print("test import");
-                            polkadotRepository.testImport();
-                          },
-                        ),
-                      ),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: WalletButtons(
-                          title: 'Test 3',
-                          onPressed: () async {
-                            // testing substrate service - leave this for now
-                            print("test 3");
-
-                            // this is one of our demo accounts with a balance on polkadot
-                            final addr = "5GwwAKFomhgd4AHXZLUBVK3B792DvgQUnoHTtQNkwmt5h17k";
-
-                            final res = await polkadotRepository.getBalance(addr);
-
-                            print("get balance result: $res");
-//                             flutter: getBalance res: {nonce: 2, consumers: 0, providers: 1, sufficients: 0, data: {free: 1499366495076723, reserved: 33333333000, miscFrozen: 0, feeFrozen: 0}}
-// flutter: get balance result: {nonce: 2, consumers: 0, providers: 1, sufficients: 0, data: {free: 1499366495076723, reserved: 33333333000, miscFrozen: 0, feeFrozen: 0}}
-                          },
-                          buttonType: ButtonsType.receiveButton,
-                        ),
-                      )
-                    ],
-                  ),
-                )
               ],
             );
           },
