1ebe4aa28515676c8ae9517995e39ed1210833a6
NIK <nheger@gmail.com>
2022-08-17 14:53:50 +0700

    fix create and remove recovery
    need to check isInBlock - same as isFinalized

--- scripts/recovery.js
+++ scripts/recovery.js
@@ -9,11 +9,18 @@ const { mnemonicGenerate } = require('@polkadot/util-crypto');
 const { hexToU8a, u8aToHex } = require("@polkadot/util");
 
 // mnemonic: someone course sketch usage whisper helmet juice oyster rebuild razor mobile announce
-const acct_0 = "5FyG1HpMSce9As8Uju4rEQnL24LZ8QNFDaKiu5nQtX6CY6BH"
+const acct_0 = process.env.G0_ADDRESS
 // mnemonic: dress teach unveil require supply move butter sort cruise divide nice account
-const acct_1 = "5Ca9Sdw7dxUK62FGkKXSZPr8cjNLobuGAgXu6RCM14aKtz6T"
+const acct_1 = process.env.G1_ADDRESS
 // mnemonic: slogan crime relief smile door make deliver staff lonely hello worry sure
-const acct_2 = "5C8126sqGbCa3m7Bsg8BFQ4arwcG81Vbbwi34EznBovrv7Zf"
+const acct_2 = process.env.G2_ADDRESS
+
+const otherAccounts = [
+  process.env.RESCUER_WORDS,
+  process.env.G0_WORDS,
+  process.env.G1_WORDS,
+  process.env.G2_WORDS,
+]
 
 const init = async () => {
   // Initialize the provider to connect to the local node
@@ -42,9 +49,12 @@ const init = async () => {
     console.error("Address incorrect: " + steve.address + " expected: " + process.env.STEVE_ADDRESS)
   }
 
-  const balance = await getBalance(steve.address, api)
-
-  console.log("steve balance: " + balance)
+  for (acct of otherAccounts) {
+    const a = keyring.addFromUri(
+      acct
+    );
+    console.log("added " + a.address)
+  }
 
   return {
     api,
@@ -57,9 +67,9 @@ const init = async () => {
 const getBalance = async (address, api) => {
   const { nonce, data: balance } = await api.query.system.account(address);
 
-  console.log(`${address}: balance of ${balance.free} and a nonce of ${nonce}`);
+  console.log(`${address}: balance of ${balance.free / 10E12} and a nonce of ${nonce}`);
 
-  return balance.free
+  return balance.free / 10E12
 }
 
 const createAccounts = async (number) => {
@@ -98,91 +108,104 @@ const findKeyring = async (address) => {
 
 }
 
+
+
 const createRecovery = async () => {
+  try {
+    const { api, keyring, steve } = await init()
 
-  const { api, keyring, steve } = await init()
+    const address = process.env.STEVE_ADDRESS
 
-  const address = "5HGZfBpqUUqGY7uRCYA6aRwnRHJVhrikn8to31GcfNcifkym"
+    console.log("balance before create recovery")
+    const balanceBefore = await getBalance(address, api);
 
-  // Note we will pass in the address, but we can easily resolve this with keyring.getPair..
-  // Alternatively we can pass in the correct keypair
+    // Note we will pass in the address, but we can easily resolve this with keyring.getPair..
+    // Alternatively we can pass in the correct keypair
 
-  let response = await new Promise(async (resolve) => {
-    const unsubscribe = await api.tx.recovery.createRecovery(
-      [
-        acct_0,
-        acct_1,
-        acct_2
-      ].sort(), 2, 0)
-      .signAndSend(keyring.getPair(address), ({ events = [], status, txHash }) => {
-        console.log(`Current status is ${status.type}`);
-  
-        if (status.isFinalized) {
-          var transactionSuccess = false
+    let response = await new Promise(async (resolve) => {
+      const unsubscribe = await api.tx.recovery.createRecovery(
+        [
+          acct_0,
+          acct_1,
+          acct_2
+        ].sort(), 2, 0)
+        .signAndSend(keyring.getPair(address), ({ events = [], status, txHash }) => {
+          console.log(`Current status is ${status.type} status.isInBlock: ` + status.isInBlock + " status.isFinalized: " + status.isFinalized);
 
-          console.log(`Transaction included at blockHash ${status.asFinalized}`);
-          console.log(`Transaction hash ${txHash.toHex()}`);
-  
-          // Loop through Vec<EventRecord> to display all events
-          events.forEach(({ phase, event: { data, method, section } }) => {
-            console.log(`\t' ${phase}: ${section}.${method}:: ${data}`);
-            if (section == "system" && method == "ExtrinsicFailed") {
-              transactionSuccess = false
-            } 
-            if (section == "system" && method == "ExtrinsicSuccess") {
-              transactionSuccess = true
-            }
-          });
+          if (status.isFinalized || status.isInBlock) {
+            var transactionSuccess = false
 
-          console.log("unsubscribing from updates..")
-          unsubscribe()
-          // => 
-          // ' {"applyExtrinsic":1}: balances.Withdraw:: ["5HGZfBpqUUqGY7uRCYA6aRwnRHJVhrikn8to31GcfNcifkym",85795211]
-          // ' {"applyExtrinsic":1}: system.ExtrinsicFailed:: [{"module":{"index":10,"error":"0x04000000"}},{"weight":148937000,"class":"Normal","paysFee":"Yes"}]
-         
-          resolve({
-            events,
-            status,
-            txHash,
-            transactionSuccess,
-          })
-  
-        }
-      });
-  });
+            console.log(`Transaction included at blockHash ${status.isFinalized ? status.asFinalized : status.asInBlock}`);
+            console.log(`Transaction hash ${txHash.toHex()}`);
 
-  //console.log("tx result " + JSON.stringify(response, null, 2))
+            // Loop through Vec<EventRecord> to display all events
+            events.forEach(({ phase, event: { data, method, section } }) => {
+              console.log(`\t' ${phase}: ${section}.${method}:: ${data}`);
+              if (section == "system" && method == "ExtrinsicFailed") {
+                transactionSuccess = false
+              }
+              if (section == "system" && method == "ExtrinsicSuccess") {
+                transactionSuccess = true
+              }
+            });
 
-  await api.disconnect()
-  console.log("disconnecting done")
+            console.log("unsubscribing from updates..")
+            unsubscribe()
+            // => 
+            // ' {"applyExtrinsic":1}: balances.Withdraw:: ["5HGZfBpqUUqGY7uRCYA6aRwnRHJVhrikn8to31GcfNcifkym",85795211]
+            // ' {"applyExtrinsic":1}: system.ExtrinsicFailed:: [{"module":{"index":10,"error":"0x04000000"}},{"weight":148937000,"class":"Normal","paysFee":"Yes"}]
 
-  return response
+            resolve({
+              events,
+              status,
+              txHash,
+              transactionSuccess,
+            })
+          }
+        });
+    });
+
+    const balanceAfter = await getBalance(address, api);
+
+    const cost = balanceBefore - balanceAfter
+    console.log("transaction cost: "+cost)
+
+    //console.log("tx result " + JSON.stringify(response, null, 2))
+
+    await api.disconnect()
+    console.log("disconnecting done")
+  } catch (err) {
+    console.log("Create recovery error: " + err)
+  }
 }
 
 const removeRecovery = async () => {
 
   const { api, keyring, steve } = await init()
 
-  const address = "5HGZfBpqUUqGY7uRCYA6aRwnRHJVhrikn8to31GcfNcifkym"
+  const address = process.env.STEVE_ADDRESS
+
+  console.log("balance before remove recovery")
+  const balanceBefore = await getBalance(address, api);
 
   // Note we will pass in the address, but we can easily resolve this with keyring.getPair..
   let response = await new Promise(async (resolve) => {
     const unsubscribe = await api.tx.recovery.removeRecovery()
       .signAndSend(keyring.getPair(address), ({ events = [], status, txHash }) => {
         console.log(`Remove Recovery: Current status is ${status.type}`);
-  
-        if (status.isFinalized) {
+
+        if (status.isFinalized || status.isInBlock) {
           var transactionSuccess = false
 
-          console.log(`Transaction included at blockHash ${status.asFinalized}`);
+          console.log(`Transaction included at blockHash ${status.isFinalized ? status.asFinalized : status.asInBlock}`);
           console.log(`Transaction hash ${txHash.toHex()}`);
-  
+
           // Loop through Vec<EventRecord> to display all events
           events.forEach(({ phase, event: { data, method, section } }) => {
             console.log(`\t' ${phase}: ${section}.${method}:: ${data}`);
             if (section == "system" && method == "ExtrinsicFailed") {
               transactionSuccess = false
-            } 
+            }
             if (section == "system" && method == "ExtrinsicSuccess") {
               transactionSuccess = true
             }
@@ -193,19 +216,25 @@ const removeRecovery = async () => {
           // => 
           // ' {"applyExtrinsic":1}: balances.Withdraw:: ["5HGZfBpqUUqGY7uRCYA6aRwnRHJVhrikn8to31GcfNcifkym",85795211]
           // ' {"applyExtrinsic":1}: system.ExtrinsicFailed:: [{"module":{"index":10,"error":"0x04000000"}},{"weight":148937000,"class":"Normal","paysFee":"Yes"}]
-         
+
           resolve({
             events,
             status,
             txHash,
             transactionSuccess,
           })
-  
+
         }
       });
   });
 
-  console.log("tx result " + JSON.stringify(response, null, 2))
+  //console.log("tx result " + JSON.stringify(response, null, 2))
+
+  console.log("balance after remove recovery")
+  const balanceAfter = await getBalance(address, api);
+
+  const cost = balanceBefore - balanceAfter
+  console.log("remove transaction cost: "+cost)
 
   await api.disconnect()
   console.log("disconnecting done")
@@ -218,7 +247,7 @@ const queryRecovery = async () => {
 
   const { api, keyring, steve } = await init()
 
-  console.log("query for steve "+steve.address)
+  console.log("query for steve " + steve.address)
 
   const recoverable = await api.query.recovery.recoverable(steve.address);
 
@@ -267,13 +296,13 @@ program
 
     const result = await init()
 
-    console.log("keypair: " + JSON.stringify(result.steve, null, 2))
-
-    const publicKey = result.steve.publicKey
-
-    const keyPair = result.keyring.getPair(u8aToHex(publicKey));
-    
-    console.log("res 2 pair "+JSON.stringify(keyPair, null, 2))
+    ///
+    /// Testing retrieval of keypar from keyring - it works...
+    ///
+    // console.log("keypair: " + JSON.stringify(result.steve, null, 2))
+    // const publicKey = result.steve.publicKey
+    // const keyPair = result.keyring.getPair(u8aToHex(publicKey));
+    // console.log("res 2 pair "+JSON.stringify(keyPair, null, 2))
 
     await result.api.disconnect();
   })
@@ -286,8 +315,6 @@ program
     console.log("Create recovery...")
 
     const result = await createRecovery()
-
-    console.log("transaction ID: " + JSON.stringify(result, null, 2))
   })
 
 program
@@ -298,13 +325,11 @@ program
     console.log("Query recovery...")
 
     const result = await queryRecovery()
-
-    console.log("result: " + JSON.stringify(result, null, 2))
   })
 
 program
   .command('query_active')
-  .description('Query recovery')
+  .description('Query active')
   .action(async function () {
 
     console.log("Query active...")
@@ -323,7 +348,7 @@ program
 
     const result = await removeRecovery()
 
-    console.log("result: " + JSON.stringify(result, null, 2))
+    //console.log("result: " + JSON.stringify(result, null, 2))
   })
 
 
