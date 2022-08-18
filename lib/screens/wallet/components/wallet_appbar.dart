98a452e13e2a6f2c0839a2aa36210a2552283438
NIK <nheger@gmail.com>
2022-08-18 21:39:48 +0700

    show address in app bar

--- lib/screens/wallet/components/wallet_appbar.dart
+++ lib/screens/wallet/components/wallet_appbar.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hashed/datasource/local/account_service.dart';
+import 'package:hashed/domain-shared/event_bus/event_bus.dart';
+import 'package:hashed/domain-shared/event_bus/events.dart';
 import 'package:hashed/domain-shared/ui_constants.dart';
 import 'package:hashed/screens/wallet/interactor/viewmodels/wallet_bloc.dart';
+import 'package:hashed/utils/short_string.dart';
 
 class WalletAppBar extends StatelessWidget implements PreferredSizeWidget {
   const WalletAppBar({super.key});
@@ -14,13 +19,34 @@ class WalletAppBar extends StatelessWidget implements PreferredSizeWidget {
     return BlocBuilder<WalletBloc, WalletState>(
       builder: (context, state) {
         return AppBar(
+          leadingWidth: 60,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Image.asset('assets/images/appbar/hashed_logo.png'),
+          ),
+          title: InkWell(
+            onTap: () => _copyToClipboard(accountService.currentAccount.address),
+            child: Row(
+              children: [
+                Text(accountService.currentAccount.address.shorter),
+                const SizedBox(
+                  width: 8,
+                ),
+                const Icon(Icons.copy),
+              ],
+            ),
+          ),
           actions: [
             const SizedBox(width: horizontalEdgePadding + 36),
-            Expanded(child: Image.asset('assets/images/appbar/hashed_logo.png', fit: BoxFit.fitHeight)),
             const SizedBox(width: horizontalEdgePadding),
           ],
         );
       },
     );
+  }
+
+  void _copyToClipboard(String words) {
+    Clipboard.setData(ClipboardData(text: words));
+    eventBus.fire(const ShowSnackBar.success('Copied Account Address'));
   }
 }
