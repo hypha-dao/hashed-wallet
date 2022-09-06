d65e567dcbaaafb2f226319267e7eaf27acffdfb
NIK <nheger@gmail.com>
2022-09-06 19:54:44 +0700

    fix app store id

--- lib/domain-shared/app_constants.dart
+++ lib/domain-shared/app_constants.dart
@@ -3,7 +3,7 @@ const String domainAppUriPrefix = 'https://hashedwallet.page.link';
 const String guardianTargetLink = 'https://app.hashed.io/vouch/?lostAccount=&rescuer=';
 const String androidPacakageName = 'io.hashed.wallet';
 const String iosBundleId = 'io.hashed.wallet';
-const String iosAppStoreId = '1507143650';
+const String iosAppStoreId = '1639248612';
 
 /// Actions
 const String transferAction = 'transfer';
