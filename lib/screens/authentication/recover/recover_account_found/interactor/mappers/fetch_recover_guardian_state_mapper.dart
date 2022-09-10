b8d441327e308c26e62ab53b94fded63681e362d
Nikolaus Heger <nheger@gmail.com>
2022-09-10 22:57:16 +0700

    fixing up mapper and variable names and types (#130)
    mapper now maps state using the config data and active recovery data

--- lib/screens/authentication/recover/recover_account_found/interactor/mappers/fetch_recover_guardian_state_mapper.dart
+++ lib/screens/authentication/recover/recover_account_found/interactor/mappers/fetch_recover_guardian_state_mapper.dart
@@ -1,4 +1,6 @@
+import 'package:hashed/datasource/local/account_service.dart';
 import 'package:hashed/datasource/local/models/account.dart';
+import 'package:hashed/datasource/remote/model/active_recovery_model.dart';
 import 'package:hashed/datasource/remote/model/guardians_config_model.dart';
 import 'package:hashed/datasource/remote/model/user_recover_model.dart';
 import 'package:hashed/domain-shared/page_state.dart';
@@ -11,37 +13,35 @@ import 'package:hashed/screens/authentication/recover/recover_account_found/reco
 
 class FetchRecoverRecoveryStateMapper extends StateMapper {
   RecoverAccountFoundState mapResultToState(RecoverAccountFoundState currentState, RecoverGuardianInitialDTO result) {
-    final List<Account> members = result.membersData;
     final Result linkResult = result.link;
-    final Result userRecoversModel = result.userRecoversModel;
-    final Result accountGuardians = result.accountGuardians;
+    final Result activeRecoveries = result.activeRecoveries;
+    final Result guardianConfig = result.guardianConfig;
 
     final Uri? link = linkResult.asValue?.value;
-    final UserRecoversModel? userRecoversModelData = userRecoversModel.asValue?.value;
-    final GuardiansConfigModel? userGuardiansModel = accountGuardians.asValue?.value;
+    final List<ActiveRecoveryModel>? userRecoversModelData = activeRecoveries.asValue?.value;
+    ActiveRecoveryModel? activeRecovery;
+    if (userRecoversModelData != null && userRecoversModelData.isNotEmpty) {
+      activeRecovery = userRecoversModelData.firstWhere((e) => e.rescuer == accountService.currentAccount.address,
+          orElse: () => userRecoversModelData.first);
+    }
+    final GuardiansConfigModel? userGuardiansModel = guardianConfig.asValue?.value;
 
-    final hasFetchedGuardians = true; // [POLKA] clean up
-    final hasGuardians = members.isNotEmpty;
+    final hasGuardians = guardianConfig.asValue?.value != null;
 
     // Check that we have all data needed from the server and it is valid.
     // We need data from multiple services and any of them can fail.
     // This is the minimum required data to proceed
-    if (hasFetchedGuardians &&
-        hasGuardians &&
-        link != null &&
-        userRecoversModelData != null &&
-        userGuardiansModel != null) {
-      final List<Account> guardians = members;
-      final confirmedGuardianSignatures = userRecoversModelData.alreadySignedGuardians.length;
+    if (hasGuardians && link != null && activeRecovery != null && userGuardiansModel != null) {
+      final List<Account> guardians = userGuardiansModel.guardianAddresses.map((e) => Account(address: e)).toList();
+      final confirmedGuardianSignatures = activeRecovery.friends.length;
 
       // check how long we have to wait before we can claim (24h delay is standard)
-      final timeLockExpirySeconds = userRecoversModelData.completeTimestamp + userGuardiansModel.delayPeriod;
+      final timeLockExpiryBlocks = activeRecovery.created + userGuardiansModel.delayPeriod;
 
       RecoveryStatus recoveryStatus;
       // for 3 signers, we need 2/3 signatures. For 4 or 5 signers, we need 3+ signatures.
-      if ((userGuardiansModel.guardians.length == 3 && confirmedGuardianSignatures >= 2) ||
-          (userGuardiansModel.guardians.length > 3 && confirmedGuardianSignatures >= 3)) {
-        if (timeLockExpirySeconds <= DateTime.now().millisecondsSinceEpoch / 1000) {
+      if (confirmedGuardianSignatures > userGuardiansModel.threshold) {
+        if (timeLockExpiryBlocks <= DateTime.now().millisecondsSinceEpoch / 1000 / 6) {
           recoveryStatus = RecoveryStatus.readyToClaimAccount;
         } else {
           recoveryStatus = RecoveryStatus.waitingFor24HourCoolPeriod;
@@ -62,10 +62,10 @@ class FetchRecoverRecoveryStateMapper extends StateMapper {
         userGuardiansData: guardians,
         confirmedGuardianSignatures: confirmedGuardianSignatures,
         recoveryStatus: recoveryStatus,
-        alreadySignedGuardians: userRecoversModelData.alreadySignedGuardians,
-        timeLockExpirySeconds: timeLockExpirySeconds,
+        alreadySignedGuardians: activeRecovery.friends,
+        timeLockExpirySeconds: timeLockExpiryBlocks,
       );
-    } else if (hasFetchedGuardians && !hasGuardians) {
+    } else if (!hasGuardians) {
       return currentState.copyWith(
         pageState: PageState.failure,
         error: RecoverAccountFoundError.noGuardians,
