74c123160351cf614544c36f557df5e5bc7c5be3
NIK <nheger@gmail.com>
2022-10-04 18:47:50 +0800

    implement recover funds

--- lib/screens/authentication/recover/recover_account_success/interactor/viewmodels/recover_account_success_bloc.dart
+++ lib/screens/authentication/recover/recover_account_success/interactor/viewmodels/recover_account_success_bloc.dart
@@ -6,6 +6,8 @@ import 'package:hashed/datasource/local/models/token_data_model.dart';
 import 'package:hashed/datasource/remote/model/balance_model.dart';
 import 'package:hashed/datasource/remote/polkadot_api/polkadot_repository.dart';
 import 'package:hashed/domain-shared/base_use_case.dart';
+import 'package:hashed/domain-shared/event_bus/event_bus.dart';
+import 'package:hashed/domain-shared/event_bus/events.dart';
 import 'package:hashed/domain-shared/page_command.dart';
 import 'package:hashed/domain-shared/page_state.dart';
 import 'package:hashed/screens/authentication/recover/recover_account_success/usecase/fetch_recover_account_success_data.dart';
@@ -39,7 +41,20 @@ class RecoverAccountSuccessBloc extends Bloc<RecoverAccountSuccessEvent, Recover
     add(const FetchInitialData());
   }
 
-  FutureOr<void> _onRecoverFundsTapped(OnRecoverFundsTapped event, Emitter<RecoverAccountSuccessState> emit) {
-    polkadotRepository.recoveryRepository.recoverAllFunds(address: event.rescuer, lostAccount: event.lostAccount);
+  FutureOr<void> _onRecoverFundsTapped(OnRecoverFundsTapped event, Emitter<RecoverAccountSuccessState> emit) async {
+    emit(state.copyWith(pageState: PageState.loading));
+
+    final res = await polkadotRepository.recoveryRepository
+        .recoverAllFunds(address: event.rescuer, lostAccount: event.lostAccount);
+
+    if (res.isValue) {
+      print("recover success ${res.asValue!.value}");
+      emit(state.copyWith(pageState: PageState.success));
+      add(const OnRefreshTapped());
+      eventBus.fire(const OnWalletRefreshEventBus());
+    } else {
+      print("recover fail with error ${res.asError!.error}");
+      emit(state.copyWith(pageState: PageState.failure));
+    }
   }
 }
