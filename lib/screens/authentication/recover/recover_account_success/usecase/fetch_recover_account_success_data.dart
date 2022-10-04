77d16899de5855aa427a6c9369cf261b30d34dac
NIK <nheger@gmail.com>
2022-10-04 20:27:20 +0800

    some error handling on the stop recovery page

--- lib/screens/authentication/recover/recover_account_success/usecase/fetch_recover_account_success_data.dart
+++ lib/screens/authentication/recover/recover_account_success/usecase/fetch_recover_account_success_data.dart
@@ -4,10 +4,6 @@ import 'package:hashed/domain-shared/base_use_case.dart';
 
 class FetchRecoverAccountSuccessDataUseCase {
   Future<Result<BalanceModel>> run(String accountName) async {
-    // TODO(Nik): This is mocked data. We need to make calls to the repo to get the data we need and Map it to a
-    // TODO(Nik): ResultData object. The bloc will know how to handle this object. If fails, return Result.error()
-
-    // What's the accountName parameter = the lost account? The rescuer account?
     final res = polkadotRepository.getBalance(accountName);
 
     return res;
