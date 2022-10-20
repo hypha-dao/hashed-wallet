5a5b4b6427c078cea3c548bbe0c863073285f8d5
NIK <nheger@gmail.com>
2022-10-20 19:18:02 +0800

    remove login screen button for recovery - that was old code

--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hashed/components/flat_button_long.dart';
-import 'package:hashed/datasource/local/account_service.dart';
-import 'package:hashed/datasource/local/settings_storage.dart';
 import 'package:hashed/navigation/navigation_service.dart';
-import 'package:hashed/utils/ThemeBuildContext.dart';
 import 'package:hashed/utils/build_context_extension.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -12,34 +9,6 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomSheet: Padding(
-        padding: const EdgeInsets.all(16),
-        child: TextButton(
-          style: TextButton.styleFrom(
-              shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))),
-          onPressed: () {
-            if (settingsStorage.inRecoveryMode) {
-              NavigationService.of(context)
-                  .navigateTo(Routes.recoverAccountDetails, arguments: accountService.currentAccount.address);
-            } else {
-              NavigationService.of(context).navigateTo(Routes.recoverAccountSearch);
-            }
-          },
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Recover your funds ',
-                style: Theme.of(context).textTheme.subtitle2,
-              ),
-              Text(
-                'here',
-                style: Theme.of(context).textTheme.subtitle2?.copyWith(color: context.colorScheme.secondary),
-              ),
-            ],
-          ),
-        ),
-      ),
       body: SafeArea(
         top: false,
         child: SingleChildScrollView(
