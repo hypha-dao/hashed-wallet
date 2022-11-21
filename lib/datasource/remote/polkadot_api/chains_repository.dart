8002120205882389758c9ab8b834939f34b24f39
Gerardo Guijarro <gguijarro_c@chewy.com>
2022-11-20 18:18:40 -0600

    Fix list

--- lib/datasource/remote/polkadot_api/chains_repository.dart
+++ lib/datasource/remote/polkadot_api/chains_repository.dart
@@ -34,6 +34,8 @@ class ChainsRepository {
 
       //print("get chains: $json");
 
+      return [];
+
       return List.of(json.map((e) => SubstrateChainContainer.fromJson(e)));
     } catch (err) {
       print("error $err");
