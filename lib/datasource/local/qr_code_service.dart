7e097b1df5a682de7eaeed73fd3bfb835872bbf1
NIK <nheger@gmail.com>
2023-01-22 20:15:31 +0800

    using ScanQrCodeResultData

--- lib/datasource/local/qr_code_service.dart
+++ lib/datasource/local/qr_code_service.dart
@@ -1,9 +1,10 @@
 import 'package:async/async.dart';
+import 'package:hashed/datasource/local/models/scan_qr_code_result_data.dart';
 import 'package:hashed/datasource/local/models/substrate_signing_request_model.dart';
 import 'package:hashed/datasource/local/signing_request_repository.dart';
 
 class QrCodeService {
-  Result<SubstrateSigningRequestModel> processQrCode(String scanResult) {
+  Result<ScanQrCodeResultData> processQrCode(String scanResult) {
     final splitUri = scanResult.split(':');
     final scheme = splitUri[0];
     if (scheme != 'ssr') {
@@ -13,7 +14,7 @@ class QrCodeService {
 
     final SubstrateSigningRequestModel? ssr = SigningRequestRepository().parseUrl(scanResult);
     if (ssr != null) {
-      return Result.value(ssr);
+      return Result.value(ScanQrCodeResultData(ssr));
     } else {
       return Result.error("Error processing QR code");
     }
