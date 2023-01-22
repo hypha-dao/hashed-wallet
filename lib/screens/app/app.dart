dce98cda5d54b9d5cb3d1ce45861e5edbde0ed2f
NIK <nheger@gmail.com>
2023-01-22 20:15:48 +0800

    remove redundant event

--- lib/screens/app/app.dart
+++ lib/screens/app/app.dart
@@ -19,7 +19,6 @@ import 'package:hashed/screens/app/interactor/viewmodels/app_bloc.dart';
 import 'package:hashed/screens/app/interactor/viewmodels/app_page_commands.dart';
 import 'package:hashed/screens/app/interactor/viewmodels/app_screen_item.dart';
 import 'package:hashed/screens/settings/settings_screen.dart';
-import 'package:hashed/screens/transfer/scan/scan_confirmation_screen.dart';
 import 'package:hashed/screens/transfer/send/send_scanner/send_scanner_screen.dart';
 import 'package:hashed/screens/wallet/wallet_screen.dart';
 
@@ -106,7 +105,7 @@ class _AppState extends State<App> with WidgetsBindingObserver {
             } else if (pageCommand is NavigateToRoute) {
               await NavigationService.of(context).navigateTo(pageCommand.route);
             } else if (pageCommand is NavigateToRouteWithArguments) {
-              if (pageCommand is NavigateToSendConfirmation) {
+              if (pageCommand is NavigateToScanConfirmation) {
                 await NavigationService.of(context).navigateTo(Routes.verificationUnpoppable).then((_) =>
                     NavigationService.of(context).navigateTo(pageCommand.route, arguments: pageCommand.arguments));
               } else {
