e083d08cf3fd2a2d829ec338f5c397f07263a6d4
NIK <nheger@gmail.com>
2023-01-27 00:53:47 +0800

    cleanup

--- lib/datasource/remote/model/chain_properties.dart
+++ lib/datasource/remote/model/chain_properties.dart
@@ -1,13 +1,12 @@
 class ChainProperties {
-  /// Note: header has a bunch of other fields, but we only use number
   final int ss58Format;
   final List<int> tokenDecimals;
   final List<String> tokenSymbol;
 
   // all the following are optional and can be null
   final String? displayName;
-  // final String? network;
-  // final int? prefix;
+  final String? network;
+  final int? prefix;
   final String? standardAccount;
   final String? website;
 
@@ -22,17 +21,16 @@ class ChainProperties {
     this.website,
   });
 
-  final String network;
-  final int prefix;
+  // DOT
+  // {ss58Format: 0, tokenDecimals: [10], tokenSymbol: [DOT]}
 
-  // json: {ss58Format: 0, tokenDecimals: [10], tokenSymbol: [DOT]}
   factory ChainProperties.fromJson(Map<String, dynamic> json) {
     return ChainProperties(
       ss58Format: json["ss58Format"],
       tokenDecimals: List<int>.from(json["tokenDecimals"]),
       tokenSymbol: List<String>.from(json["tokenSymbol"]),
-      displayName: json["displayName"], // can be null
-      network: json["network"], // can be null
+      displayName: json["displayName"],
+      network: json["network"],
       prefix: json["prefix"],
       standardAccount: json["standardAccount"],
       website: json["website"],
